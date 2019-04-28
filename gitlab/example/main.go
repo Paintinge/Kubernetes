af12204459a55e4c5f4667f3d34a45792df75070
Stranded <473439438@qq.com>
2019-04-28 15:36:26 +0800

    main.go

---
+++ gitlab/example/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc(
+		"/hello",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello!")
+		},
+	)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
